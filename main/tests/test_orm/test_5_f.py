ae2c41572e76402d933c3cb86e3b429021ccc806
andy7 <vinandy4718@gmail.com>
2025-05-05 14:56:51 +0300

    5 Задание, проходят все тесты

--- main/tests/test_orm/test_5_f.py
+++ main/tests/test_orm/test_5_f.py
@@ -1,11 +1,14 @@
 from django.db.models import F, Q
 
 from .base import BaseORMTestCase
+from ...models import Bookmark
 
 
 class TestF(BaseORMTestCase):
     def test_f(self):
         # Получить список закладок, у которых url НЕ совпадает с названием (т.е. НЕ равны), используя Q() и F()
-        bookmarks = ...  # TODO
+        bookmarks = Bookmark.objects.filter(
+            ~Q(url__exact=F('title'))
+        ).values_list('title', flat=True)
 
         self.assertEquals(self.BOOKMARK_TITLES, list(bookmarks.values_list('title', flat=True)))
