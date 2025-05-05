bc808fe0965eb263271d06a619629763c74d0d89
andy7 <vinandy4718@gmail.com>
2025-05-05 15:13:30 +0300

    6 Задание, проходят все тесты

--- main/tests/test_orm/test_6_aggregate.py
+++ main/tests/test_orm/test_6_aggregate.py
@@ -1,4 +1,7 @@
+from django.db.models import Sum, Max, Avg, Min, Count
+
 from .base import BaseORMTestCase
+from ...models import Group
 
 
 class TestAggregate(BaseORMTestCase):
@@ -6,25 +9,25 @@ class TestAggregate(BaseORMTestCase):
 
     def test_sum(self):
         # Получить сумму значение поля order всех групп
-        result = ...  # TODO
+        result = Group.objects.aggregate(Sum('order'))
         self.assertEquals(11, result['order__sum'])
 
     def test_count(self):
         # Получить кол-во имён всех групп, используя аггрегирующую функцию
-        result = ...  # TODO
+        result = Group.objects.aggregate(Count('name'))
         self.assertEquals(5, result['name__count'])
 
     def test_min(self):
         # Получить минимальный order среди групп
-        result = ...  # TODO
+        result = Group.objects.aggregate(Min('order'))
         self.assertEquals(0, result['order__min'])
 
     def test_max(self):
         # Получить максимальный order среди групп
-        result = ...  # TODO
+        result = Group.objects.aggregate(Max('order'))
         self.assertEquals(5, result['order__max'])
 
     def test_avg(self):
         # Получить среднее арифметическое для order
-        result = ...  # TODO
+        result = Group.objects.aggregate(Avg('order'))
         self.assertEquals(2.2, result['order__avg'])
